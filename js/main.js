5c45ed036e5fd41143b9e4bfa1088beb81d299dc
Veronica Enamorado <veronica.enamorado@gmail.com>
2019-07-22 23:22:57 +0200

    Fix spider appearing after game over

--- js/main.js
+++ js/main.js
@@ -12,25 +12,21 @@ const move = (event) => {
   if(event.keyCode ===  38) {
     if (characterTop > 3) {
       characterTop -=20;
-      console.log('characterTop', characterTop);
       character.style.top = characterTop + 'px';
     }
   } else if (event.keyCode === 39) {
     if (characterLeft < 540) {
       characterLeft +=20;
-      console.log('characterLeft', characterLeft);
       character.style.left = characterLeft + 'px';
     }
   } else if (event.keyCode === 40) {
     if (characterTop < 423) {
       characterTop +=20;
-      console.log('characterTop', characterTop);
       character.style.top = characterTop + 'px';
     }
     } else if (event.keyCode === 37) {
     if (characterLeft > 0) {
       characterLeft -=20;
-      console.log('characterLeft', characterLeft);
       character.style.left = characterLeft + 'px';
     }
   }
@@ -51,14 +47,16 @@ const appearSpider = () => {
 const eatSpider = () => {
   if (characterTop >= 0 && characterTop <= 100 && characterLeft >= 50 && characterLeft <= 160 ) {
     spider.classList.add('hidden');
-    spider.classList.add('alert');
+    spider.classList.add('stay');
   }
 }
 
 const game = (event) => {
   move (event);
   eatPumpkin();
-  appearSpider();
+  if (!spider.classList.contains('stay')) {
+    appearSpider();
+  }
   eatSpider();
 }
 
